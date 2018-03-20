ac5150945a8287e6116aa43a149d58f2b0ef392c
Suellen Aline de Paula <suellen.alinep@gmail.com>
2018-03-19 21:10:11 -0300

    FileCabinet

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,19 +1,21 @@
 class DocsController < ApplicationController
 
 	before_action :find_doc, only: [:show, :edit, :update, :destroy]
+	
 	def index
-		@docs = Doc.all.order('created_at DESC')
+		@docs = Doc.where(user_id: current_user)
 	end
 
 	def show
 	end
 
 	def new
-		@doc = Doc.new
-	end
+		@doc = current_user.docs.build
 
+	end
+		
 	def create
-		@doc = Doc.new(doc_params)
+		@doc = current_user.docs.build(doc_params)
 
 		if @doc.save
 			redirect_to docs_path
@@ -27,7 +29,7 @@ class DocsController < ApplicationController
 	end
 
 	def update
-		if @doc.update(doc.params)
+		if @doc.update(doc_params)
 			redirect_to docs_path
 		else
 			render 'edit'
